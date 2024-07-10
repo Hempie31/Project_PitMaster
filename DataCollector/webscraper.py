02f4c78f75c33945f30834190483073a25720b5e
Alexandros Hemphill <alexhill2k1@gmail.com>
2024-07-09 20:59:50 -0400

    Converted numbers to integers, sorted dataframe by number

--- DataCollector/webscraper.py
+++ DataCollector/webscraper.py
@@ -16,9 +16,13 @@ df = tables[0].drop([0, 1])
 
 df = df.rename(columns={'Unnamed: 0': 'Number', 'Unnamed: 1': 'Name', 'Unnamed: 2': 'Nationality', 'Unnamed: 3': 'Team', 'Unnamed: 4': 'Constructor'})
 
+for i in df['Number']:
+    df.replace(to_replace=i, value=int(i), inplace=True)
+
+df = df.sort_values(by=['Number'])
 
 # Display the extracted table
-print (df.columns)
+# print (df.columns)
 print(df)
 
 
