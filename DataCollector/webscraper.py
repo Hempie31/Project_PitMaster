c9f507065db9cfddff4d7dc0f699a786a37a0e52
Alexandros Hemphill <alexhill2k1@gmail.com>
2024-07-17 16:10:40 -0400

    Implemented the updating of a drivers laptimes

--- DataCollector/webscraper.py
+++ DataCollector/webscraper.py
@@ -30,28 +30,55 @@ class WebScraper:
                      'Unnamed: 2': 'Nationality',
                      'Unnamed: 3': 'Team',
                      'Unnamed: 4': 'Constructor'})
+        self.driverInformationDF["lapTimes"] = {}
 
-        self.driverInformationDF["lapTimes"] = ""
-
-        for i in self.driverInformationDF['Number']:
-            self.driverInformationDF.replace(to_replace=i, value=int(i),
+        for driver_number in self.driverInformationDF['Number']:
+            self.driverInformationDF.replace(to_replace=driver_number,
+                                             value=int(driver_number),
                                              inplace=True)
 
-        self.driverInformationDF = self.driverInformationDF.sort_values(
-            by=['Number']
-            )
+        self.driverInformationDF = self.driverInformationDF. \
+            sort_values(by=['Number'])
 
         # Display the extracted table
-        print(self.driverInformationDF)
+        print("\n", self.driverInformationDF)
 
     def extract_lap_info(self):
+
+        driver_full_names = list(self.driverInformationDF["Name"])
+        # driver_surnames = []
+        # for full_name in driver_full_names:
+        #     name_list = full_name.split(" ")
+        #     driver_surnames.append(name_list[-1])
 
         print("Extracting lap info...")
         # driverNames = self.driverInformationDF['Name'].tolist()
         lap_time_data = parser.from_file(LAP_TIMES_URL)
         lap_time_data_list = lap_time_data["content"].split("\n")
 
+        temporary_lap_times = []
+        temporary_driver = ""
         for line in lap_time_data_list:
             split_line = line.split(" ")
+
             if (len(split_line) == 3):
-                print(line, "\t", len(split_line))
+                if (split_line[1] != ""):
+                    found_name = search_list_for_name(split_line[2],
+                                                      driver_full_names)
+
+                    if found_name is not None:
+                        # if (temporary_driver != ""):
+                        self.driverInformationDF.loc[
+                            self.driverInformationDF["Name"] == found_name,
+                            'lapTimes'
+                        ] = {'lapTimes': {'laps': [4, 5, 6]}}
+                # print(split_line[0], "\t", split_line[1], "\t", split_line[2])
+        print(self.driverInformationDF)
+
+
+def search_list_for_name(search_string, driver_names):
+
+    for name in driver_names:
+        if search_string.lower() in name.split(" ")[-1].lower():
+            return (name)
+    return None
