4c2c44420e5fa49f2d24c4f2d98a50bd56ca8823
Alexandros Hemphill <alexhill2k1@gmail.com>
2024-07-22 16:35:45 -0400

    Added URL validator

--- DataCollector/main.py
+++ DataCollector/main.py
@@ -3,5 +3,6 @@ import webscraper
 
 if __name__ == "__main__":
     webscraperObject = webscraper.WebScraper()
-    webscraperObject.extract_driver_info()
-    webscraperObject.extract_lap_info()
+    # webscraperObject.extract_driver_info()
+    # webscraperObject.extract_lap_info()
+    webscraperObject.iterate_through_races()
--- DataCollector/webscraper.py
+++ DataCollector/webscraper.py
@@ -3,18 +3,41 @@ from tika import parser
 import numpy
 import tabula
 from datetime import datetime
+import urllib.request
+import requests
 
 
-DRIVER_INFO_URL = "https://www.fia.com/sites/default/files/" \
-                "doc_15_-_2022_saudi_arabian_grand_prix_-_entry_list.pdf"
-LAP_TIMES_URL = "https://www.fia.com/sites/default/files/" \
-                "2022_02_ksa_f1_r0_timing_racelapanalysis_v01.pdf"
+DRIVER_INFO_URL = "https://www.fia.com/sites/default/files/doc_15_-_2022_saudi_arabian_grand_prix_-_entry_list.pdf"
+LAP_TIMES_URL = "https://www.fia.com/sites/default/files/2022_02_ksa_f1_r0_timing_racelapanalysis_v01.pdf"
+STARTING_YEAR = 2015
+CURRENT_YEAR = int(datetime.today().strftime('%Y'))
 
 
 class WebScraper:
 
     def __init__(self):
         self.driverInformationDF = {}
+
+    def iterate_through_races(self):
+        # fp = urllib.request.urlopen("https://www.fia.com/events/fia-formula-one-world-championship/season-2023/2023-fia-formula-one-world-championship")
+        fp = urllib.request.urlopen("https://www.fia.com/events/fia-formula-one-world-championship/season-2016/formula-one")
+        # print(check_url("https://www.fia.com/events/fia-formula-one-world-championship/season-2021/calendar"))
+        # print(check_url("https://www.fia.com/events/fia-formula-one-world-championship/season-20212/calendar"))
+
+        mybytes = fp.read()
+
+        mystr = mybytes.decode("utf8")
+        fp.close()
+
+        race_list = []
+        contains_error = False
+        for line in mystr.split("\n"):
+            if ("grand-prix" in line and "<li>" not in line and "news" not in line):
+                html_string_split = line.split('"')
+
+                if (len(html_string_split) == 7):
+                    print(html_string_split[3])
+                    race_list.append(line)
 
     def extract_driver_info(self):
 
@@ -34,6 +57,7 @@ class WebScraper:
             'Unnamed: 4': 'Constructor'})
         self.driverInformationDF["lapTimes"] = self.driverInformationDF.apply(lambda x: [], axis=1)
         self.driverInformationDF["pitstops"] = self.driverInformationDF.apply(lambda x: [], axis=1)
+        self.driverInformationDF["tiresUsed"] = self.driverInformationDF.apply(lambda x: [], axis=1)
 
         for driver_number in self.driverInformationDF['Number']:
             self.driverInformationDF.replace(to_replace=driver_number, value=int(driver_number), inplace=True)
@@ -86,3 +110,21 @@ def search_list_for_name(search_string, driver_names):
         if search_string.lower() in name.split(" ")[-1].lower():
             return (name)
     return None
+
+
+def check_url(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
+        print(f"Successfully loaded: {url}")
+        return True
+    except requests.exceptions.HTTPError as http_err:
+        if response.status_code == 404:
+            print(f"Failed to load resource: the server responded with a status of 404 ({url})")
+            return False
+        else:
+            print(f"HTTP error occurred: {http_err} ({url})")
+            return False
+    except Exception as err:
+        print(f"Other error occurred: {err} ({url})")
+        return False
