aa446994a9208939c037f71db12aab4bed37d6eb
AHemphill <alexhill2k1@gmail.com>
2024-08-09 08:38:59 -0400

    Added Database Insertions

--- DataCollector/main.py
+++ DataCollector/main.py
@@ -1,8 +1,11 @@
 import webscraper
 
+DATABASE_URI = "mongodb+srv://admin:admin@f1-cluster.bn1crlr.mongodb.net/?retryWrites=true&w=majority&appName=F1-Cluster"
+
 
 if __name__ == "__main__":
     webscraperObject = webscraper.WebScraper()
     # webscraperObject.extract_driver_info()
     # webscraperObject.extract_lap_info()
-    webscraperObject.iterate_through_races()
+    # for i in range(2019, 2024, 1):
+    webscraperObject.iterate_through_races(2022)
--- DataCollector/webscraper.py
+++ DataCollector/webscraper.py
@@ -2,42 +2,135 @@
 from tika import parser
 import numpy
 import tabula
+import pymongo
 from datetime import datetime
-import urllib.request
+from collections import defaultdict
+import time
 import requests
+import fastf1
+import sys
 
+DATABASE_URI = "mongodb+srv://admin:admin@f1-cluster.bn1crlr.mongodb.net/?retryWrites=true&w=majority&appName=F1-Cluster"
 
 DRIVER_INFO_URL = "https://www.fia.com/sites/default/files/doc_15_-_2022_saudi_arabian_grand_prix_-_entry_list.pdf"
 LAP_TIMES_URL = "https://www.fia.com/sites/default/files/2022_02_ksa_f1_r0_timing_racelapanalysis_v01.pdf"
-STARTING_YEAR = 2015
+STARTING_YEAR = 2016
 CURRENT_YEAR = int(datetime.today().strftime('%Y'))
+
+fastf1.Cache.enable_cache('FastF1-Cache')
 
 
 class WebScraper:
 
     def __init__(self):
+        self._mongo_client = pymongo.MongoClient(DATABASE_URI)
         self.driverInformationDF = {}
 
-    def iterate_through_races(self):
-        # fp = urllib.request.urlopen("https://www.fia.com/events/fia-formula-one-world-championship/season-2023/2023-fia-formula-one-world-championship")
-        fp = urllib.request.urlopen("https://www.fia.com/events/fia-formula-one-world-championship/season-2016/formula-one")
-        # print(check_url("https://www.fia.com/events/fia-formula-one-world-championship/season-2021/calendar"))
-        # print(check_url("https://www.fia.com/events/fia-formula-one-world-championship/season-20212/calendar"))
+    def iterate_through_races(self, year):
+        database = self._mongo_client["F1-Lap-Data"]
+        collection = database[str(year)]
 
-        mybytes = fp.read()
+        # try:
+            # Attempt to load the event schedule for the given year
+        schedule = fastf1.get_event_schedule(year)
+        if schedule.empty:
+            print(f"No schedule data available for {year}.")
+        else:
+            print(f"\nEvent schedule for {year}:\n")
 
-        mystr = mybytes.decode("utf8")
-        fp.close()
+            more_races = True
+            race_number = 1
+            while (more_races):
+                # try:
+                event_name = schedule.get_event_by_round(race_number)["EventName"]
+                session_type = 'R'
+                race_dict = {"Name": event_name, "Drivers": {}}
 
-        race_list = []
-        contains_error = False
-        for line in mystr.split("\n"):
-            if ("grand-prix" in line and "<li>" not in line and "news" not in line):
-                html_string_split = line.split('"')
+                session = get_session_with_retry(year, event_name, session_type)
 
-                if (len(html_string_split) == 7):
-                    print(html_string_split[3])
-                    race_list.append(line)
+                # Get laps data
+                laps = session.laps
+                results = session.results
+
+                # Extract pit stop data
+                pit_stops = laps[laps['PitOutTime'].notnull() | laps['PitInTime'].notnull()]
+
+                lap_dict = defaultdict(list)
+                # Print pit stop times
+                offset = False
+                lap_in_info = ()
+                for _, pit_stop in pit_stops.iterrows():
+                    driver = pit_stop['Driver']
+                    pit_in = pit_stop['PitInTime']
+                    pit_out = pit_stop['PitOutTime']
+                    lap_number = pit_stop['LapNumber']
+
+                    if (offset):
+                        if (driver == lap_in_info[0]):
+                            pit_lap_in = laps[(laps['Driver'] == driver) & (laps['LapNumber'] == lap_in_info[1])]
+                            if not pit_lap_in.empty:
+                                old_compound = pit_lap_in.iloc[0]['Compound']
+
+                            pit_lap_out = laps[(laps['Driver'] == driver) & (laps['LapNumber'] == lap_number)]
+                            if not pit_lap_out.empty:
+                                new_compound = pit_lap_out.iloc[0]['Compound']
+
+                            lap_dict[driver].append({"lap": lap_in_info[1], "duration": (pit_out - lap_in_info[2]), "oldTire": old_compound, "newTire": new_compound})
+                            offset = False
+                        else:
+                            lap_in_info = (driver, lap_number, pit_in)
+                    else:
+                        lap_in_info = (driver, lap_number, pit_in)
+                        offset = True
+
+                # print(lap_dict["GAS"])
+                for driver_num in session.drivers:
+                    driver_details = session.get_driver(driver_num)
+
+                    driver_code = driver_details['Abbreviation']
+                    driver_name = driver_details['FullName']
+                    position = driver_details['Position']
+                    race_dict["Drivers"][driver_name] = {}
+
+                    driver_result = results[results['Abbreviation'] == driver_code]
+                    if not driver_result.empty:
+                        status = driver_result.iloc[0]['Status']
+
+                    laps = session.laps.pick_driver(driver_code)
+                    lap_numbers = laps['LapNumber'].tolist()
+                    lap_times = laps['LapTime'].tolist()
+                    sector1_times = laps['Sector1Time'].tolist()
+                    sector2_times = laps['Sector2Time'].tolist()
+                    sector3_times = laps['Sector3Time'].tolist()
+
+                    race_dict["Drivers"][driver_name]["Laps"] = {}
+                    race_dict["Drivers"][driver_name]["PitStops"] = {}
+                    race_dict["Drivers"][driver_name]["Team"] = driver_details['TeamName']
+                    race_dict["Drivers"][driver_name]["Finishing Position"] = position
+                    race_dict["Drivers"][driver_name]["Race Status"] = status
+                    for i in range(len(lap_numbers)):
+                        time_obj = {'lap_time': str(lap_times[i]),
+                                    'sector_1': str(sector1_times[i]),
+                                    'sector_2': str(sector2_times[i]),
+                                    'sector_3': str(sector3_times[i])}
+                        race_dict["Drivers"][driver_name]["Laps"][f"lap {i+1}"] = time_obj
+
+                    for i in range(len(lap_dict[driver_code])):
+                        pit_obj = {'duration': str(lap_dict[driver_code][i]['duration']),
+                                    'oldTire': lap_dict[driver_code][i]['oldTire'],
+                                    'newTire': lap_dict[driver_code][i]['newTire']
+                                    }
+                        race_dict["Drivers"][driver_name]["PitStops"][f"lap {lap_dict[driver_code][i]['lap']}"] = pit_obj
+
+                collection.insert_one(race_dict)
+                race_number += 1
+
+                    # except (ValueError):
+                    #     print("round does not exist in " + str(year))
+                    #     more_races = False
+
+        # except Exception as e:
+        #     print(f"Could not retrieve schedule for {year}: {e}")
 
     def extract_driver_info(self):
 
@@ -100,8 +193,8 @@ class WebScraper:
 
         print("\n", self.driverInformationDF)
 
-        for time in self.driverInformationDF.loc[18, 'lapTimes']:
-            print(time)
+        # for time in self.driverInformationDF.loc[18, 'lapTimes']:
+        #     print(time)
 
 
 def search_list_for_name(search_string, driver_names):
@@ -128,3 +221,18 @@ def check_url(url):
     except Exception as err:
         print(f"Other error occurred: {err} ({url})")
         return False
+
+
+def get_session_with_retry(year, grand_prix, session_type, retries=3, delay=5):
+    for attempt in range(retries):
+        try:
+            session = fastf1.get_session(year, grand_prix, session_type)
+            session.load()
+            return session
+        except requests.exceptions.HTTPError as e:
+            if e.response.status_code == 503:
+                print(f"Attempt {attempt + 1} failed with 503 error. Retrying in {delay} seconds...")
+                time.sleep(delay)
+            else:
+                raise
+    raise Exception(f"Failed to load session after {retries} attempts")
